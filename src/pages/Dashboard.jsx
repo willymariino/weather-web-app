d65a97b031f821417fe79e18072d4feae060a2ae
willymariino <wg.mariino@gmail.com>
2026-03-09 20:07:01 +0100

    feat: test on endpoint

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,4 +1,26 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 function DashBoard() {
+
+    // endpoint API open-meteo.com
+    // coordinate Lodi: latitude=45.3128 longitude=9.4978
+    const endPoint = "https://api.open-meteo.com/v1/forecast?latitude=45.3128&longitude=9.4978&current=temperature_2m";
+
+    function getWeather() {
+
+        axios.get(endPoint)
+            .then(res => {
+                console.log(res.data)
+
+            })
+            .catch(err => {
+                console.log(err)
+            })
+
+    }
+
+    useEffect(getWeather, [])
 
     return (
         <>
