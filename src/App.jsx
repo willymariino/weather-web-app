11005a9845a37cab4a9820813ae19b2cf452b81d
willymariino <wg.mariino@gmail.com>
2026-03-09 19:48:47 +0100

    feat: use pages and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import DashBoard from "./pages/Dashboard"
+import Settings from "./pages/Settings"
+import DefaultLayout from "./layouts/defaultLayout"
+
 function App() {
 
 
   return (
     <>
+
+      <BrowserRouter>
+
+        <Routes>
+
+          <Route Component={DefaultLayout}>
+
+            <Route index Component={DashBoard} />
+            <Route path="/" Component={DashBoard} />
+            <Route path="settings" Component={Settings} />
+
+          </Route>
+
+
+        </Routes>
+
+
+      </BrowserRouter>
 
     </>
   )
