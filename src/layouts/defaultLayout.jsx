fb096abfb0f16773252e20bdf49668e6bdb19a62
willymariino <wg.mariino@gmail.com>
2026-03-09 19:30:13 +0100

    feat: add DefaultLayout

---
+++ src/layouts/defaultLayout.jsx
@@ -0,0 +1,14 @@
+import Header from "../components/header";
+import { Outlet } from "react-router-dom";
+
+function DefaultLayout() {
+
+    return (
+        <>
+            <Header />
+            <Outlet />
+        </>
+    )
+}
+
+export default DefaultLayout
