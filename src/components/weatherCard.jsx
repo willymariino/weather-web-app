e2fe35919141b9927873f2b657ef05ceb8265fc9
willymariino <wg.mariino@gmail.com>
2026-03-11 02:22:54 +0100

    feat: format weather timestamp for display

--- src/components/weatherCard.jsx
+++ src/components/weatherCard.jsx
@@ -2,13 +2,20 @@ import getWeatherDescription from "../utility/weatherCodes"
 
 function WeatherCard({ weather }) {
 
+    const formattedDate = new Date(weather.current.time).toLocaleDateString("it-IT", {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+        year: "numeric"
+    });
+
     return (
         <>
             <p>  Temperature: {weather.current.temperature_2m} {weather.current_units.temperature_2m} </p>
             <p>Umidità: {weather.current.relative_humidity_2m}{weather.current_units.relative_humidity_2m} </p>
             <p>velocità del vento: {weather.current.wind_speed_10m} {weather.current_units.wind_speed_10m}</p>
             <p>Cielo: {getWeatherDescription(weather.current.weathercode)}</p>
-            <p> Data rilevazione: {weather.current.time}</p>
+            <p> Data rilevazione: {formattedDate}</p>
 
         </>
     )
